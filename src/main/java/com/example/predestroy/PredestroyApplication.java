892ad9241b7c292181a61fb9dd5d0ebc574f42dd
corekms <corekms@gmail.com>
2022-08-31 18:46:22 +0900

    protype Bean의 PreDestroy 동작 확인

--- src/main/java/com/example/predestroy/PredestroyApplication.java
+++ src/main/java/com/example/predestroy/PredestroyApplication.java
@@ -7,15 +7,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class PredestroyApplication {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext ctx = 
-				new AnnotationConfigApplicationContext();
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(MyConfiguration.class);
+		ctx.refresh();
 
 		MyBean mb1 = ctx.getBean(MyBean.class);
 		System.out.println(mb1.hashCode());
 
 		MyBean mb2 = ctx.getBean(MyBean.class);
 		System.out.println(mb2.hashCode());
-
+		
 		ctx.close();
 	}
 
